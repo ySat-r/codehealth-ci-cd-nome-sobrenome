c8a8d616ca931d773b94cd95a4892a8bd0357bdb
10410051 <226614468+10410051@users.noreply.github.com>
2025-11-07 23:38:14 +0000

    [M]App.java -> Implementaçao do Menu

--- App.java
+++ App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hi");
+
+        System.out.println("---Menu---");
+        System.out.println("1. 1");
+        System.out.println("2. 2");
+        System.out.println("3. 3");
+
+        Scanner sc = new Scanner(System.in);
+        switch (sc.nextInt()) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+
+            default:
+                System.out.println("0");
+                break;
+        }
     }
 }
